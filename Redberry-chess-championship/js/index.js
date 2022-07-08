d2d62b4a15b4844cdb188f24d6de7c9a46e08e4e
lizibartaia <bartaializi76@gmail.com>
2022-07-08 19:07:13 +0400

    second-page

--- Redberry-chess-championship/js/index.js
+++ Redberry-chess-championship/js/index.js
@@ -1,3 +1,7 @@
 document.getElementById("getstarted").onclick = function () {
     location.href = "index-next.html";
 };
+
+document.getElementsByClassName("back").onclick = function () {
+    location.href = "index.html";
+};
